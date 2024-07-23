10eb71d706476d877208f16c1b7c9ac611c09745
Mykyta Yeremenko <nikr3enox@gmail.com>
2024-07-23 18:09:08 +0300

    Add company form

---
+++ src/app/components/AddCompanyButton.tsx
@@ -0,0 +1,19 @@
+'use client';
+import React, { useState } from 'react';
+import Button from './Button';
+import dynamic from 'next/dynamic';
+// import CompanyFormModal from './CompanyFormModal';
+
+const CompanyFormModal = dynamic(() => import('./CompanyFormModal'), { ssr: false });
+
+const AddCompanyButton = () => {
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <Button onClick={() => setShow(true)}>Add Company</Button>
+      <CompanyFormModal onSubmit={console.log} show={show} onClose={() => setShow(false)} />
+    </>
+  );
+};
+
+export default AddCompanyButton;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
-import Image from 'next/image';
-import StatusLable, { Status } from './components/status-lable';
+import AddCompanyButton from './components/AddCompanyButton';
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
-      <StatusLable status={Status.Active}>Active</StatusLable>
-      <StatusLable status={Status.NotActive}>Not Active</StatusLable>
-      <StatusLable status={Status.Pending}>Pending</StatusLable>
-      <StatusLable status={Status.Suspended}>Suspended</StatusLable>
+      <AddCompanyButton />
     </main>
   );
 }
